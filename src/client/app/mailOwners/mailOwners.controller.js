7a44d6427c9ab323e03a23bc49c1b6c7e91ebc1d
Edward Bradley <edbrad@edbrad.com>
2015-12-19 20:43:40 -0600

    Display Mail Owners using UI-Grid

--- src/client/app/mailOwners/mailOwners.controller.js
+++ src/client/app/mailOwners/mailOwners.controller.js
@@ -2,16 +2,30 @@
     'use strict';
 
     angular
-        .module('app.mailOwners', ['lbServices'])
+        .module('app.mailOwners', ['lbServices', 'ui.grid', 'ui.grid.pagination', 'ui.grid.resizeColumns'])
         .controller('MailOwnersController', MailOwnersController);
 
-    MailOwnersController.$inject = ['$q', 'MailOwner', 'logger'];
+    MailOwnersController.$inject = ['$q', 'MailOwner', 'logger', '$scope'];
     /* @ngInject */
-    function MailOwnersController($q, MailOwner, logger) {
+    function MailOwnersController($q, MailOwner, logger, $scope) {
         var vm = this;
         vm.title = 'Mail Owners';
         
         vm.mailOwners = [];
+        
+        $scope.gridOptions = {
+            paginationPageSizes: [25, 75, 100],
+            columnDefs:[
+                {name: 'Name'},
+                {name: 'Address1'},
+                {name: 'Address2'},
+                {name: 'City' },
+                {name: 'State'},
+                {name: 'Zip5' }
+            ],
+            enableGridMenu: true,
+            enableFiltering: true
+        };
         
         activate();
         
@@ -26,6 +40,7 @@
             MailOwner.find(
                 function (result) {
                     vm.mailOwners = result;
+                    $scope.gridOptions.data = result;
                 });
         }
     }

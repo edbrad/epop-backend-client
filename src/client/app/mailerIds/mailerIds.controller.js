f73a590b4dbd7bbc087c90f6f472a337f062beb0
Edward Bradley <edbrad@edbrad.com>
2015-12-20 01:21:51 -0600

    Display Mailer ID's using UI-Grid

--- src/client/app/mailerIds/mailerIds.controller.js
+++ src/client/app/mailerIds/mailerIds.controller.js
@@ -2,17 +2,33 @@
     'use strict';
 
     angular
-        .module('app.mailerIds', ['lbServices'])
+        .module('app.mailerIds', ['lbServices', 'ui.grid', 'ui.grid.pagination', 'ui.grid.resizeColumns'])
         .controller('MailerIdsController', MailerIdsController);
 
-    MailerIdsController.$inject = ['$q', 'MailerID', 'MailOwner', 'logger'];
+    MailerIdsController.$inject = ['$q', 'MailerID', 'MailOwner', 'logger', '$scope'];
     /* @ngInject */
-    function MailerIdsController($q, MailerID, MailOwner, logger) {
+    function MailerIdsController($q, MailerID, MailOwner, logger, $scope) {
         var vm = this;
         vm.title = 'Mailer Ids';
         
         vm.MailerIds = [];
         vm.MailOwners = [];
+        
+        $scope.gridOptions = {
+            paginationPageSizes: [25, 75, 100],
+            columnDefs:[
+                {name: 'MailerID', displayName: 'Mailer ID'},
+                {field: 'mailOwnerId', name: 'mailOwnerId', displayName: 'Mail Owner Name',
+                 cellTemplate: '<div style="padding: 5px;">{{grid.appScope.getMailOwnerName(row.entity.mailOwnerId)}}</div>'},
+                {name: 'MailerAddress1', displayName: 'Mailer Address 1'},
+                {name: 'MailerAddress2', displayName: 'Mailer Address 2'},
+                {name: 'MailerCity', displayName: 'Mailer City'},
+                {name: 'MailerState', displayName: 'Mailer State'},
+                {name: 'MailerZip5', displayName: 'Zip'}
+            ],
+            enableGridMenu: true,
+            enableFiltering: true
+        };
         
         activate();
         
@@ -27,6 +43,7 @@
             MailerID.find(
                 function (result) {
                     vm.MailerIds = result;
+                    $scope.gridOptions.data = result;
                 });
         }
         
@@ -37,7 +54,16 @@
                 });
         }
         
-        vm.getMailOwnerName = function(id){
+        /*vm.getMailOwnerName = function(id){
+            for(var i = 0 ; i < vm.MailOwners.length; i++){
+                var obj = vm.MailOwners[i];
+                if (obj.id == id){
+                    return obj.Name;
+                }
+            }
+        };*/
+        
+        $scope.getMailOwnerName = function(id){
             for(var i = 0 ; i < vm.MailOwners.length; i++){
                 var obj = vm.MailOwners[i];
                 if (obj.id == id){

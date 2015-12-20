462c9848a853377e056db98d7013b81ae0371573
Edward Bradley <edbrad@edbrad.com>
2015-12-19 23:52:00 -0600

    Display CRIDs using UI-Grid

--- src/client/app/CRIDs/crids.controller.js
+++ src/client/app/CRIDs/crids.controller.js
@@ -2,17 +2,28 @@
     'use strict';
 
     angular
-        .module('app.CRIDs', ['lbServices'])
+        .module('app.CRIDs', ['lbServices', 'ui.grid', 'ui.grid.pagination', 'ui.grid.resizeColumns'])
         .controller('CRIDsController', CRIDsController);
 
-    CRIDsController.$inject = ['$q', 'CRID', 'MailOwner', 'logger'];
+    CRIDsController.$inject = ['$q', 'CRID', 'MailOwner', 'logger', '$scope'];
     /* @ngInject */
-    function CRIDsController($q, CRID, MailOwner, logger) {
+    function CRIDsController($q, CRID, MailOwner, logger, $scope) {
         var vm = this;
         
         vm.title = 'CRIDs';
         vm.CRIDs = [];
         vm.MailOwners = [];
+        
+        $scope.gridOptions = {
+            paginationPageSizes: [25, 75, 100],
+            columnDefs:[
+                {field: 'mailOwnerId', name: 'mailOwnerId', displayName: 'Mail Owner Name',
+                 cellTemplate: '<div style="padding: 5px;">{{grid.appScope.getMailOwnerName(row.entity.mailOwnerId)}}</div>'},
+                {name: 'CRID', displayName: 'CRID'},
+            ],
+            enableGridMenu: true,
+            enableFiltering: true
+        };
         
         activate();
         
@@ -27,6 +38,7 @@
             CRID.find(
                 function (result) {
                     vm.CRIDs = result;
+                    $scope.gridOptions.data = result;
                 });
         }
         
@@ -37,7 +49,16 @@
                 });
         }
         
-        vm.getMailOwnerName = function(id){
+        /*vm.getMailOwnerName = function(id){
+            for(var i = 0 ; i < vm.MailOwners.length; i++){
+                var obj = vm.MailOwners[i];
+                if (obj.id == id){
+                    return obj.Name;
+                }
+            }
+        };*/
+        
+        $scope.getMailOwnerName = function(id){
             for(var i = 0 ; i < vm.MailOwners.length; i++){
                 var obj = vm.MailOwners[i];
                 if (obj.id == id){
@@ -45,6 +66,6 @@
                 }
             }
         };
-        
+         
     }
 })();

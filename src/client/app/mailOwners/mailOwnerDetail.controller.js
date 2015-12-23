9a3bb451f6faf47b41081cace75330038b337ca5
edb@edbrad.com <edb@edbrad.com>
2015-12-23 16:39:31 -0600

    Initial Google Maps work

--- src/client/app/mailOwners/mailOwnerDetail.controller.js
+++ src/client/app/mailOwners/mailOwnerDetail.controller.js
@@ -2,25 +2,50 @@
     'use strict';
 
     angular
-        .module('app.mailOwnerDetail', ['lbServices', 'app.dialogsService', 'ui.grid', 'ui.grid.pagination', 'ui.grid.resizeColumns', 'ui.grid.moveColumns', 'ui.grid.selection', 'ui.grid.exporter'])
+        .module('app.mailOwnerDetail', 
+            [
+            'lbServices', 
+            'app.dialogsService', 
+            'ui.grid', 
+            'ui.grid.pagination', 
+            'ui.grid.resizeColumns', 
+            'ui.grid.moveColumns', 
+            'ui.grid.selection', 
+            'ui.grid.exporter',
+            'ui.Gmapgoogle-maps'
+            ]
+        )
         .controller('MailOwnerDetailController', MailOwnerDetailController);
 
-    MailOwnerDetailController.$inject = ['$q', 'MailOwner', 'logger', '$scope', '$stateParams', 'dialogsService'];
+    MailOwnerDetailController.$inject = ['$q', 'MailOwner', 'logger', '$scope', '$stateParams', 'dialogsService','uiGmapGoogleMapApi'];
     /* @ngInject */
-    function MailOwnerDetailController($q, MailOwner, logger, $scope, $stateParams, dialog) {
+    function MailOwnerDetailController($q, MailOwner, logger, $scope, $stateParams, dialog, uiGmapGoogleMapApi) {
         var vm = this;
         vm.title = 'Mail Owner Detail';
         vm.mailOwner = [];
         
         vm.click = function(){
-            dialog.confirm('Are you sure about what you are doing?', 'Confirm?', ['OK', 'CANCEL'])
+            dialog.confirm('Are you sure about what you are doing?', 'Confirm?', ['YES', 'CANCEL'])
             .then(function(){
                 logger.info("modal complete!");
             });
         };
         
-        vm.formatCityStateZip = function(city, state, zip5, zip4){
-            return city + ', ' + state + ' ' + zip5;
+        vm.formatCityStateZip = function(city, state, zip5, zipPlus4){
+            if (zipPlus4){   
+                return city + ', ' + state + ' ' + zip5 + '-' + zipPlus4;
+            }
+            else{
+                return city + ', ' + state + ' ' + zip5;
+            }   
+        };
+        
+        vm.map = {
+            center:{
+                latitude: 38.897733,
+                longitude: -77.036531
+            },
+            zoom: 12    
         };
         
         activate();
